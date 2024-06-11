dcdd8e8b807c89c7d01150129d1b3ae5af8a1c3b
adeyemoadeniran <niranadeniran1@gmail.com>
2024-06-11 17:19:48 +0100

    Added the Arabic numerals to Roman numerals functionality.

--- script.js
+++ script.js
@@ -3,6 +3,40 @@ const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 const form = document.querySelector('form');
 
+function arabicNumToRomanNum(input) {
+  const romanMap = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  // Recursive function
+  function convert(remainder) {
+    if (remainder === 0) {
+      return '';
+    }
+
+    for (const key in romanMap) {
+      const value = romanMap[key];
+      if (remainder >= value) {
+        return key + convert(remainder - value);
+      }
+    }
+  }
+
+  return convert(input);
+}
+
 function checkUserInput() {
   const inputInt = parseInt(numberInput.value);
 
@@ -23,6 +57,22 @@ function checkUserInput() {
       '<h2 class="result-text">Please enter a number greater than or equal to 1</h2>';
     return;
   }
+
+  if (numberInput.value >= 4000) {
+    errorStyle();
+
+    output.innerHTML =
+      '<h2 class="result-text">Please enter a number less than or equal to 3999</h2>';
+    return;
+  }
+
+  resetOuput();
+
+  output.innerHTML = `<h2 class="result-text">${arabicNumToRomanNum(
+    inputInt
+  )}</h2>`;
+
+  numberInput.value = '';
 }
 
 form.addEventListener('submit', (e) => {
@@ -35,6 +85,15 @@ function errorStyle() {
   output.style.backgroundColor = 'lightpink';
   output.style.color = '#a01414';
   output.style.border = '5px solid #a01414';
+
+  numberInput.value = '';
+  numberInput.focus();
+}
+
+function resetOuput() {
+  output.style.backgroundColor = '#ffffff37';
+  output.style.color = '#fff';
+  output.style.border = '3px solid #fff';
 
   numberInput.value = '';
   numberInput.focus();
